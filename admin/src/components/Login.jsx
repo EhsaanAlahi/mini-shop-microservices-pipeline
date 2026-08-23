a5916d0a192e990996b951d77f5d3317bdad38ab
FaisalMagray21 <fmagray287@gmail.com>
2026-08-23 21:51:00 +0500

    test commit

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>here is a checked login page</div>
+    <div>here is a checked login page heleeor ioejdoisjdsojsdoisdjosdjsd Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio accusantium magnam illo placeat architecto, aspernatur non! Eum nisi dolor similique architecto mollitia quasi dignissimos deserunt, tempore, dolore blanditiis modi, necessitatibus excepturi repellat ab maiores id sunt inventore quos? Quidem ratione dolore pariatur soluta sunt.</div>
   )
 }
 
